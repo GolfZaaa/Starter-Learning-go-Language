925fe8eb2e10340123ae4bc31019505d6633e1b3
GolfZaaa <you@example.com>
2023-11-17 20:39:31 +0700

    Display Data and Display Type

--- program.go
+++ program.go
@@ -15,7 +15,23 @@ import "fmt"
 // }
 
 // นิยามค่าคงที่
+// func main() {
+// 	const name string = "Avirut Chai"
+// 	fmt.Println("My Name is", name)
+// }
+
 func main() {
-	const name string = "Avirut Chai"
-	fmt.Println("My Name is", name)
+	name := "Avirut Chaisongkram"
+	age := 25
+	score := 95.5
+	pass := true
+	fmt.Printf("My Name is %v\n", name)
+	fmt.Printf("Type name = %T\n", name)
+	fmt.Printf("Age is %v\n", age)
+	fmt.Printf(" Type Age = %T\n", age)
+	fmt.Printf(" Score is %v\n", score)
+	fmt.Printf(" Type Score = %T\n", score)
+	fmt.Printf(" Pass is = %v\n", pass)
+	fmt.Printf(" Type pass = %T\n", pass)
+
 }
