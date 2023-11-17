416da09d245ac8be999d5bcdc61f13c09d78bdb4
GolfZaaa <you@example.com>
2023-11-17 16:12:00 +0700

    Definition of Variables and Constants

--- program.go
+++ program.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+// นิยามตัวแปร
+// func main() {
+// 	name := "Avirut Chaisongkram"
+// 	age := 25
+// 	score := 95.5
+// 	pass := true
+// 	fmt.Println("My Name is", name)
+// 	fmt.Println("Age is", age)
+// 	fmt.Println("Score is", score)
+// 	fmt.Println("Pass Excemple is", pass)
+// }
+
+// นิยามค่าคงที่
 func main() {
-	fmt.Println("Hello, world!")
-	fmt.Println("Golf Za")
+	const name string = "Avirut Chai"
+	fmt.Println("My Name is", name)
 }
